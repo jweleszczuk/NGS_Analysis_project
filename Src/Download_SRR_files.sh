d3cf07889e028efef92ecb48bf9c8febd27c45a8
Jarosław <82106367+jweleszczuk@users.noreply.github.com>
2022-05-29 23:49:43 +0200

    Update Download_SRR_files.sh

--- Src/Download_SRR_files.sh
+++ Src/Download_SRR_files.sh
@@ -8,7 +8,7 @@ begin_name="SRR0645"
 
 end_name=".fastq.gz"
 
-folder_name="home/jarek/Project_NGS/SRR_"
+folder_name="Project_NGS/SRR_"
 
 for number in ${l_of_names[*]};do
 	fastq-dump  SRR0645$number -X 1000000  --split-files $folder_name$number
