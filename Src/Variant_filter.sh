5655de6ac516a9b50b12ef37663257fb2a7ad087
Jarosław <82106367+jweleszczuk@users.noreply.github.com>
2022-05-29 22:01:07 +0200

    Add files via upload

---
+++ Src/Variant_filter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+l_of_names_1=(45 46 47 "Call_all")
+
+for number in ${l_of_names_1[*]};do
+
+	vcftools --gzvcf $number".vcf.gz" --remove-indels --max-missing 0.8 --minGQ 20  --recode-INFO-all --recode  --out $number"_filtered"
+
+done
+
