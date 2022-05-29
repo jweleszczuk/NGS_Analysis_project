d3b70f51b96872677baab2512ea2c6350b378a24
Jarosław <82106367+jweleszczuk@users.noreply.github.com>
2022-05-29 16:36:16 +0200

    Add files via upload
    Download SRR repositories.

---
+++ Src/Download_SRR_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+l_of_names=(45 46 47)
+
+begin_name="SRR0645"
+
+end_name=".fastq.gz"
+
+folder_name="home/jarek/Project_NGS/SRR_"
+
+for number in ${l_of_names[*]};do
+	fastq-dump  SRR0645$number -X 1000000  --split-files $folder_name$number
+
+done
+
