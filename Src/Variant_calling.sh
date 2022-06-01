4adac4f18d789bdd73604f7b81ebfe758fa2ed3d
Jarosław <82106367+jweleszczuk@users.noreply.github.com>
2022-06-01 20:19:30 +0200

    Update Variant_calling.sh

--- Src/Variant_calling.sh
+++ Src/Variant_calling.sh
@@ -2,12 +2,6 @@
 
 samtools faidx "Project_NGS/chromosomes/whole_genom.fa"
 
-bcftools mpileup  -Ou --fasta-ref  "Project_NGS/chromosomes/whole_genom.fa" "Project_NGS/mapped/SRR_45/45_hisat_sorted.bam" "Project_NGS/mapped/SRR_46/46_hisat_sorted.bam" "Project_NGS/mapped/SRR_47/47_hisat_sorted.bam" > "Project_NGS/mapped/my_raw.bcf" 
-
-
-bcftools call "Project_NGS/mapped/my_raw.bcf" -vmO z -o "Project_NGS/mapped/Call_all.vcf.gz"
-
-
 l_of_names_1=(45 46 47)
 
 for number in ${l_of_names_1[*]};do
