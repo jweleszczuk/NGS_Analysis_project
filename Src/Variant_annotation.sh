8e3744730e8b03326c51f17773cd18b6495e5478
Jarosław <82106367+jweleszczuk@users.noreply.github.com>
2022-06-01 20:18:58 +0200

    Update Variant_annotation.sh

--- Src/Variant_annotation.sh
+++ Src/Variant_annotation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-l_of_names_1=(45 46 47 "Call_all")
+l_of_names_1=(45 46 47 )
 path_dyd="Project_NGS/mapped/VCF/"
 for number in ${l_of_names_1[*]};do	
 	java -Xmx4g -jar "snpEff/snpEff.jar" -c "snpEff/snpEff.config" R64-1-1.105 $path_dyd$number"_filtered.recode.vcf" > $path_dyd$number"_annotaed.EFF.vcf"
